e512832df6f39965818cd78fc52d9c441608fd7d
crosspostly <shekhovpavel@gmail.com>
2025-11-16 16:41:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
- plugins: [
-   tailwindcss(),
-   react(),
-   nodePolyfills(),
- ],
- optimizeDeps: {
-   esbuildOptions: {
-     define: {
-       global: 'globalThis'
-     }
-   }
- },
- server: {
-   proxy: {
-     '/api': {
-       target: 'http://localhost:3000',
-       changeOrigin: true,
-       secure: false,
-     }
-   }
- }
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
